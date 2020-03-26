4e029fe7be4308964badcfd307ce84101ead997e
Victoria-Butrym <victoria-butrym@tut.by>
2020-03-26 14:04:37 +0300

    feat: implement encode function

--- task_1/app.js
+++ task_1/app.js
@@ -1,4 +1,14 @@
 const { program } = require('commander');
+const fs = require('fs');
+// const path = require('path');
+
+const encode = require('./cipher');
+
+const textStream = fs.createReadStream(`${__dirname}/text.txt`, 'utf8');
+
+textStream.on('data', chunk => {
+  console.log(encode(chunk, 1));
+});
 
 program.version('0.0.1');
 
