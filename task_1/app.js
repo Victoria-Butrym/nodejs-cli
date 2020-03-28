c72f4ff43f270df23ca1e6b7df7690ed3d1459a0
Victoria-Butrym <victoria-butrym@tut.by>
2020-03-28 16:01:35 +0300

    feat: use stdout as an output destination, if output filename is missed

--- task_1/app.js
+++ task_1/app.js
@@ -10,9 +10,17 @@ program.version('0.0.1');
 
 program
   .option('-s, --shift <number>', 'position shift') // make required
-  .option('-i, --input <string>', 'input file')
-  .option('-o, --output <string>', 'output file')
-  .requiredOption('-a, --action <string>', 'encode/decode'); // make required
+  .option('-i, --input <filename>', 'input file')
+  .option('-o, --output <filename>', 'output file')
+  .option('-a, --action [type]', 'encode/decode'); // make required
+// .action(() => {
+//   const options = program.opts();
+
+//   if (options.action !== 'encode' || options.action !== 'decode') {
+//     process.stderr.write('Error: Invalid action');
+//     process.exit(1);
+//   }
+// });
 
 program.parse(process.argv);
 
@@ -24,17 +32,31 @@ const options = {
 const inputFile = path.basename(program.input);
 const outputFile = path.basename(program.output);
 
-console.log(options, inputFile, outputFile);
+// console.log(options, inputFile, outputFile);
 
-const inputStream = fs.createReadStream(path.join(__dirname, inputFile), {
-  flags: 'r',
-  encoding: 'utf8'
-});
-const outputStream = fs.createWriteStream(path.join(__dirname, outputFile), {
-  flags: 'a'
-});
+let inputStream;
+let outputStream;
 
-pipeline(inputStream, new CaesarStream(options), outputStream);
+if (program.input) {
+  inputStream = fs.createReadStream(path.join(__dirname, inputFile), {
+    encoding: 'utf8'
+  });
+} else {
+  inputStream = process.stdin;
+  inputStream.on('data', data => data.toString());
+}
+
+if (process.output) {
+  outputStream = fs.createWriteStream(path.join(__dirname, outputFile), {
+    flags: 'a'
+  });
+} else {
+  outputStream = process.stdout;
+}
+
+pipeline(inputStream, new CaesarStream(options), outputStream, err =>
+  process.stderr.write('Damn! Check your files, bro...', err)
+);
 
 // fs.createReadStream(path.join(__dirname, inputFile), {
 //   flags: 'r',
@@ -58,9 +80,9 @@ pipeline(inputStream, new CaesarStream(options), outputStream);
 //       })
 //   );
 
-if (program.input === undefined) {
-  process.stdin.write('Enter something: ');
-}
+// if (program.input === undefined) {
+//   process.stdin.write('Enter something: ');
+// }
 
 // if (program.input) {
 //   encodeFile(inputStream, +program.shift);
@@ -77,4 +99,4 @@ if (program.input === undefined) {
 // // TODO-SAT:если переданы все аргументы, приложение читает из файла и записывает в файл зашифрованный/расшифрованный текст, при этом предыдущие записи не удаляются
 // TODO-SAT:если переданы аргументы с путями к файлам, но файлы отсутствуют (или к ним невозможен доступ), приложение передает соответствующее сообщение в process.stderr и прoцесс завершается с кодом, отличным от 0
 // TODO-SUN:если не передан аргумент с путем до файла на чтение, то чтение осуществляется из process.stdin
-// TODO-SUN:если не передан аргумент с путем до файла на запись, то вывод осуществляется в process.stdout
+// // TODO-SUN:если не передан аргумент с путем до файла на запись, то вывод осуществляется в process.stdout
